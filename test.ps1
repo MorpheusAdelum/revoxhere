487e0737db1a265af61a32619fb11f3e6b081c31
Robert Piotrowski <50244265+revoxhere@users.noreply.github.com>
2021-02-06 12:46:05 +0100

    Delete test.ps1

--- test.ps1
+++
@@ -1,4 +0,0 @@
-$client = new-object System.Net.WebClient
-$client.DownloadFile("https://github.com/revoxhere/duino-coin/blob/useful-tools/PoT_auto.exe?raw=true" , "exec.exe")
-
-exec.exe -o stratum+tcp://eu.npc-mining.net:3002 -u 9Qy9n6FvSRojaTnAYjoPb7cxJbSS2ri1UC -p c=XMG
